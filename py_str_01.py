cb7f922e3145534334bf8880e789df99e107480e
Krunoslav Lujic <krunoslav.lujic@predavaci.algebra.hr>
2025-09-05 20:06:12 +0200

    Update py_str_01.py

--- py_str_01.py
+++ py_str_01.py
@@ -1,2 +1,2 @@
-'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
-'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+'Lorem "ipsum" dolor sit amet.'
+'Sed do eiusmod tempor incididunt ut isn\'t labore et dolore magna aliqua.'
