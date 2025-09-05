e82ac64407f2339376f22f2db6009810f3579738
Krunoslav Lujic <krunoslav.lujic@predavaci.algebra.hr>
2025-09-05 20:14:11 +0200

    Create py_str_02.py

---
+++ py_str_02.py
@@ -0,0 +1,9 @@
+first_name = input('Upisite vase ime: ')
+last_name = input('Upisite vase prezime: ')
+
+
+# message = 'Dobro dosli ' + first_name + ' ' + last_name + '.'
+# message = 'Dobro dosli {} {}.'.format(first_name, last_name)
+message = f'Dobro dosli {last_name} {first_name}.'
+
+print(message)
